0abe7c45104073b4a87aec99f302e97e1f03a356
har-y <jakub.harenda@gmail.com>
2020-05-20 22:28:23 +0200

    Add: Script - Brick Shape Script - Brick Shape Move / Rotate

--- Assets/Scripts/Brick Tetris/BrickShape.cs
+++ Assets/Scripts/Brick Tetris/BrickShape.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class BrickShape : MonoBehaviour
 {
+    public bool canRotate = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void BrickMove(Vector3 direction)
+    {
+        transform.position += direction;
+    }
+
+    public void MoveLeft()
+    {
+        BrickMove(new Vector3(-1f, 0f, 0f));
+    }
+
+    public void MoveRight()
+    {
+        BrickMove(new Vector3(1f, 0f, 0f));
+    }
+
+    public void MoveDown()
+    {
+        BrickMove(new Vector3(0f, -1f, 0f));
+    }
+
+    public void MoveUp()
+    {
+        BrickMove(new Vector3(0f, 1f, 0f));
+    }
+
+    public void RotateLeft()
+    {
+        if (canRotate)
+        {
+            transform.Rotate(0f, 0f, 90f);
+        }
+    }
+
+    public void RotateRight()
+    {
+        if (canRotate)
+        {
+            transform.Rotate(0f, 0f, -90f);
+        }
     }
 }
