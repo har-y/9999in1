b56012dc7dc97a3c2cc681880e458e929c28d9b8
har-y <jakub.harenda@gmail.com>
2020-05-21 00:03:23 +0200

    Add: Script - Spawner Script - Spawn Shape / Random Shape

--- Assets/Scripts/Brick Tetris/Spawner.cs
+++ Assets/Scripts/Brick Tetris/Spawner.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public BrickShape[] brickShapes;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,36 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private BrickShape GetRandomShape()
+    {
+        int i = Random.Range(0, brickShapes.Length);
+
+        if (brickShapes[i])
+        {
+            return brickShapes[i];
+        }
+        else
+        {
+            Debug.Log("invalid shape");
+            return null;
+        }
+    }
+
+    private BrickShape SpawnShape()
+    {
+        BrickShape shape = null;
+        shape = Instantiate(GetRandomShape(), transform.position, Quaternion.identity) as BrickShape;
+
+        if (shape)
+        {
+            return shape;
+        }
+        else
+        {
+            Debug.Log("invalid shape");
+            return null;
+        }
     }
 }
