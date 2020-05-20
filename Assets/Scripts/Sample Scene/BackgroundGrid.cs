2fcbc5c52a04540fc7ec2b8f7ea78a7272cab258
har-y <jakub.harenda@gmail.com>
2020-05-20 23:28:43 +0200

    Add: Script - Background Grid Script - Create Grid

--- Assets/Scripts/Sample Scene/BackgroundGrid.cs
+++ Assets/Scripts/Sample Scene/BackgroundGrid.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class BackgroundGrid : MonoBehaviour
 {
+    public Transform brick;
+    private Transform[,] _grid;
+
+    public int height = 27;
+    public int width = 11;
+
+
+    private void Awake()
+    {
+        _grid = new Transform[width, height];
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        DrawEmptyCells();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void DrawEmptyCells()
+    {
+        if (brick != null)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Transform clone;
+                    clone = Instantiate(brick, new Vector3(x, y, 0), Quaternion.identity) as Transform;
+                    clone.name = "Grid Space ( x = " + x.ToString() + " , y =" + y.ToString() + " )";
+                    clone.transform.parent = transform;
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("assign sprite object!");
+
+        }
+
     }
 }
