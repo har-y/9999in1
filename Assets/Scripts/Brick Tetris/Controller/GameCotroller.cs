69dd9812d548e07829bd12c371ad1428151e11d4
har-y <jakub.harenda@gmail.com>
2020-05-24 01:33:09 +0200

    Add: Script - Game Controller Script - Shape Move Right

--- Assets/Scripts/Brick Tetris/Controller/GameCotroller.cs
+++ Assets/Scripts/Brick Tetris/Controller/GameCotroller.cs
@@ -10,12 +10,17 @@ public class GameCotroller : MonoBehaviour
     //info:: current shape
     private BrickShape _activeShape;
 
-    private float _interval = 1f;
+    private float _timeInterval = 1f;
     private float _time;
+    private float _timeNextKey;
+
+    [Range(0.02f, 1f)] public float _timeRepeatRateKey = 0.25f;
 
     // Start is called before the first frame update
     void Start()
     {
+        _timeNextKey = Time.time;
+
         _backgroundGrid = FindObjectOfType<BackgroundGrid>();
         if (!_backgroundGrid)
         {
@@ -27,20 +32,47 @@ public class GameCotroller : MonoBehaviour
         {
             Debug.Log("not assign object");
         }
-
-        if (_spawner)
+        else
         {
-            if (_activeShape == null)
+            _spawner.transform.position = Vectorf.Round(_spawner.transform.position);
+
+            if (!_activeShape)
             {
                 _activeShape = _spawner.SpawnShape();
             }
-            _spawner.transform.position = Vectorf.Round(_spawner.transform.position);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!_backgroundGrid || _spawner || _activeShape)
+        {
+            return;
+        }
+
+        PlayerInput();
+    }
+
+    private void PlayerInput()
+    {
+        if (Input.GetButton("MoveRight") && Time.time > _timeNextKey || Input.GetButtonDown("MoveRight"))
+        {
+            _activeShape.MoveRight();
+            _timeNextKey = Time.time + _timeRepeatRateKey;
+
+            if (_backgroundGrid.IsValidPosition(_activeShape))
+            {
+                Debug.Log("move right");
+            }
+            else
+            {
+                _activeShape.MoveLeft();
+
+                Debug.Log("hit the right boundary");
+            }
+        }
+
         if (!_backgroundGrid || !_spawner)
         {
             return;
@@ -48,7 +80,7 @@ public class GameCotroller : MonoBehaviour
 
         if (Time.time > _time)
         {
-            _time = Time.time + _interval;
+            _time = Time.time + _timeInterval;
 
             if (_activeShape)
             {
