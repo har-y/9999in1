9315f12752d18836782b65ea2ec80886d53ba9dd
har-y <jakub.harenda@gmail.com>
2020-05-26 23:06:56 +0200

    Add: Script - Audio Manager Script - Play Background Music

--- Assets/Scripts/Brick Tetris/Controller/AudioManager.cs
+++ Assets/Scripts/Brick Tetris/Controller/AudioManager.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    [Range(0f, 1f)] public float musicVolume = 1f;
+    [Range(0f, 1f)] public float fxVolume = 1f;
+
+    public bool musicEnabled = true;
+    public bool fxEnabled = true;
+
+    public AudioClip clearRowSound;
+    public AudioClip moveSound;
+    public AudioClip dropSound;
+    public AudioClip gameOverSound;
+
+    public AudioSource musicSource;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +27,19 @@ public class AudioManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void PlayBackgroundMusic(AudioClip musicClip)
+    {
+        if (!musicEnabled || !musicClip || !musicSource)
+        {
+            return;
+        }
+
+        musicSource.Stop();
+        musicSource.clip = musicClip;
+        musicSource.volume = musicVolume;
+        musicSource.loop = true;
+        musicSource.Play();
     }
 }
