6b30e09d1297177b057ae412b56fd8b3bed485fc
har-y <jakub.harenda@gmail.com>
2020-05-28 21:32:31 +0200

    Add: Script - Screen Fader Script

---
+++ Assets/Scripts/Brick Tetris/Utility/ScreenFader.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenFader : MonoBehaviour
+{
+    public float startAlpha = 1f;
+    public float targetAlpha = 0f;
+
+    private float currentAlpha;
+    private float _increment;
+
+    public float delay = 0f;
+    public float timeToFade = 1f;
+
+    private MaskableGraphic _graphic;
+    private Color originalColor;
+
+    // Use this for initialization
+    void Start()
+    {
+        _graphic = GetComponent<MaskableGraphic>();
+
+        originalColor = _graphic.color;
+
+        currentAlpha = startAlpha;
+
+        Color tempColor = new Color(originalColor.r, originalColor.g, originalColor.b, currentAlpha);
+        _graphic.color = tempColor;
+
+        _increment = ((targetAlpha - startAlpha) / timeToFade) * Time.deltaTime;
+
+        StartCoroutine(FadeRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    IEnumerator FadeRoutine()
+    {
+        yield return new WaitForSeconds(delay);
+
+        while (Mathf.Abs(targetAlpha - currentAlpha) > 0.01f)
+        {
+            yield return new WaitForEndOfFrame();
+
+            currentAlpha = currentAlpha + _increment;
+
+            Color tempColor = new Color(originalColor.r, originalColor.g, originalColor.b, currentAlpha);
+            _graphic.color = tempColor;
+        }
+
+        Debug.Log("screen fader end");
+    }
+}
