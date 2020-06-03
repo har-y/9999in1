5811a676c8946edf50b293d39dc6f30d0c0b0db1
har-y <jakub.harenda@gmail.com>
2020-06-04 00:28:34 +0200

    Fix: Animation - Clear Brick Rows Animation (1)

--- Assets/Scripts/Brick Tetris/Utility/ClearAnimation.cs
+++ Assets/Scripts/Brick Tetris/Utility/ClearAnimation.cs
@@ -9,7 +9,7 @@ public class ClearAnimation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
