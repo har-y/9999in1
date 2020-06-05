8251cc6a8164497bcb22cdb8b01f93cf053150c0
har-y <jakub.harenda@gmail.com>
2020-06-05 23:50:51 +0200

    Add: Script - Touch Controller Script - Swipe

--- Assets/Scripts/Brick Tetris/Controller/TouchController.cs
+++ Assets/Scripts/Brick Tetris/Controller/TouchController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class TouchController : MonoBehaviour
 {
+    public delegate void TouchEventHandler(Vector2 swipe);
+
+    public static event TouchEventHandler SwipeEvent;
+
+    private Vector2 _touchMovement;
+
+    private int _minimumSwipeDistance = 20;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,30 @@ public class TouchController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.touches[0];
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                _touchMovement = Vector2.zero;
+            }
+            else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            {
+                _touchMovement += touch.deltaPosition;
+                if (_touchMovement.magnitude > _minimumSwipeDistance)
+                {
+                    OnSwipe();
+                }
+            }
+        }
+    }
+
+    private void OnSwipe()
+    {
+        if (SwipeEvent != null)
+        {
+            SwipeEvent(_touchMovement);
+        }
     }
 }
