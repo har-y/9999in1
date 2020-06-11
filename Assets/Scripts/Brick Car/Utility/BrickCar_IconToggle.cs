afdb837d9867b7471c0ddfb23bacb4d36ae91600
har-y <jakub.harenda@gmail.com>
2020-06-11 23:59:23 +0200

    Add: Script - Icon Toggle Script

---
+++ Assets/Scripts/Brick Car/Utility/BrickCar_IconToggle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class BrickCar_IconToggle : MonoBehaviour
+{
+    public Sprite icon_true;
+    public Sprite icon_false;
+
+    public bool defaultSoundState = true;
+
+    private Image _image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _image = GetComponent<Image>();
+        _image.sprite = (defaultSoundState) ? icon_true : icon_false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleIcon(bool state)
+    {
+        if (!_image || !icon_true || !icon_false)
+        {
+            Debug.Log("missing sound icon");
+            return;
+        }
+
+        _image.sprite = (state) ? icon_true : icon_false;
+    }
+}
