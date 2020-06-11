89fa4643d76fb4b67fd23ce7f645b353a0cfd5ed
har-y <jakub.harenda@gmail.com>
2020-06-11 23:08:19 +0200

    Add: Script - Brick Shape Script

---
+++ Assets/Scripts/Brick Car/Core/BrickCar_BackgroundGrid.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickCar_BackgroundGrid : MonoBehaviour
+{
+    public Transform brick;
+
+    public int height = 35;
+    public int width = 11;
+    public int header = 8;
+
+    private Transform[ , ] _grid;
+
+    private void Awake()
+    {
+        _grid = new Transform[width, height];       
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DrawEmptyCells();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void DrawEmptyCells()
+    {
+        if (brick != null)
+        {
+            for (int y = 0; y < height - header; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Transform clone;
+                    clone = Instantiate(brick, new Vector3(x, y, 0), Quaternion.identity) as Transform;
+                    clone.name = "Grid Space ( x = " + x.ToString() + " , y =" + y.ToString() + " )";
+                    clone.transform.parent = transform;
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("assign sprite object!");
+
+        }
+    }
+}
---
+++ Assets/Scripts/Brick Car/Core/BrickCar_BrickShape.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickCar_BrickShape : MonoBehaviour
+{
+    public Vector3 _queuedOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void BrickMove(Vector3 direction)
+    {
+        transform.position += direction;
+    }
+
+    public void MoveLeft()
+    {
+        BrickMove(new Vector3(-1f, 0f, 0f));
+    }
+
+    public void MoveRight()
+    {
+        BrickMove(new Vector3(1f, 0f, 0f));
+    }
+
+    public void MoveDown()
+    {
+        BrickMove(new Vector3(0f, -1f, 0f));
+    }
+
+    public void MoveUp()
+    {
+        BrickMove(new Vector3(0f, 1f, 0f));
+    }
+}
