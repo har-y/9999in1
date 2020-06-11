3e5081cd1a977f5b124f4fcacfb735b906f80f27
har-y <jakub.harenda@gmail.com>
2020-06-11 23:38:07 +0200

    Add: Script - Spawner Script

---
+++ Assets/Scripts/Brick Car/Core/BrickCar_Spawner.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickCar_Spawner : MonoBehaviour
+{
+    public Transform queuedForms;
+    public BrickCar_BrickShape[] brickShapes;
+
+    private BrickCar_BrickShape _queuedShape;
+
+    private float _queueScale = 0.76f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitQueue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private BrickCar_BrickShape GetRandomShape()
+    {
+        int i = Random.Range(0, brickShapes.Length);
+
+        if (brickShapes[i])
+        {
+            return brickShapes[i];
+        }
+        else
+        {
+            Debug.Log("invalid shape");
+            return null;
+        }
+    }
+
+    public BrickCar_BrickShape SpawnShape()
+    {
+        BrickCar_BrickShape shape = null;
+
+        shape = GetQueuedShape();
+
+        shape.transform.position = transform.position;
+        shape.transform.localScale = Vector3.one;
+
+        if (shape)
+        {
+            return shape;
+        }
+        else
+        {
+            Debug.Log("invalid shape");
+            return null;
+        }
+    }
+
+    private void InitQueue()
+    {
+        _queuedShape = null;
+
+        FillQueue();
+    }
+
+    private void FillQueue()
+    {
+        if (!_queuedShape)
+        {
+            _queuedShape = Instantiate(GetRandomShape(), transform.position, Quaternion.identity);
+
+            _queuedShape.transform.position = queuedForms.position + _queuedShape._queuedOffset;
+            _queuedShape.transform.localScale = new Vector3(_queueScale, _queueScale, _queueScale);
+        }
+    }
+
+    private BrickCar_BrickShape GetQueuedShape()
+    {
+        BrickCar_BrickShape firstShape = null;
+
+        if (_queuedShape)
+        {
+            firstShape = _queuedShape;
+        }
+
+        _queuedShape = null;
+
+        FillQueue();
+
+        return firstShape;
+    }
+}
